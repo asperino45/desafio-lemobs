54d5f2e1edbae76780241e4c569bb96ddf31b3e2
asperino45 <juan.merinojr@gmail.com>
2019-07-31 08:26:26 -0300

    mudando variável debug de dev do BD

--- config/config.js
+++ config/config.js
@@ -1,6 +1,13 @@
 module.exports = {
   development: {
-    use_env_variable: 'BD_URI'
+    username: process.env.BD_USUARIO,
+    password: process.env.BD_SENHA,
+    database: process.env.BD_NOME,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    dialectOptions: {
+      appName: process.env.BD_NOME_APLICACAO
+    }
   },
   test: {
     username: process.env.BD_USUARIO,
