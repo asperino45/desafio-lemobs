8e4626e18c1268a3e2c04f97b3e4ffc7da065ac9
asperino45 <juan.merinojr@gmail.com>
2019-07-31 08:09:34 -0300

    filtro de bairro;bugfix: modelo endereço

--- api/controllers/endereco.js
+++ api/controllers/endereco.js
@@ -1,5 +1,6 @@
 const Aluno = require('../models').Aluno;
 const Endereco = require('../models').Endereco;
+const Op = require('sequelize').Op;
 
 async function cadastrarEndereco(req, res) {
     return Endereco
@@ -7,14 +8,21 @@ async function cadastrarEndereco(req, res) {
             rua: req.body.rua,
             numero: req.body.numero || "s/n",
             complemento: req.body.complemento,
-            bairro: req.body.bairro
+            bairro: req.body.bairro,
+            aluno_id: req.body.aluno_id
         })
         .then((endereco) => res.status(201).send(endereco))
         .catch((error) => res.status(400).send(error));
 }
 
 async function listarEnderecos(req, res) {
-    return Endereco.findAll()
+    return Endereco.findAll(req.swagger.params.bairro.value ? {
+        where: {
+            bairro: {
+                [Op.iLike]: req.swagger.params.bairro.value
+            }
+        }
+    } : {})
         .then((enderecos) => {
             if (!enderecos) {
                 return res.status(404).send({
